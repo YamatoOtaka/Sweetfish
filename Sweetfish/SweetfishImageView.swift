2e4e5b51c9dda9ff0899d8e40304055642d41164
Yamato Otaka <yamato8010apple@gmail.com>
2020-04-15 22:16:44 +0900

    Add sweetfishImageView

---
+++ Sweetfish/SweetfishImageView.swift
@@ -0,0 +1,13 @@
+//
+//  SweetfishImageView.swift
+//  Sweetfish
+//
+//  Created by 大高倭 on 2020/04/15.
+//  Copyright © 2020 YamatoOtaka. All rights reserved.
+//
+
+import UIKit
+
+public final class SweetfishImageView: UIImageView {
+}
+
