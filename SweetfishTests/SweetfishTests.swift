6aab9b9d16f68e08e701ef1d22c97cfd2bfdf560
Yamato Otaka <yamato8010apple@gmail.com>
2020-04-15 16:05:35 +0900

    Initial commit

---
+++ SweetfishTests/SweetfishTests.swift
@@ -0,0 +1,34 @@
+//
+//  SweetfishTests.swift
+//  SweetfishTests
+//
+//  Created by 大高倭 on 2020/04/15.
+//  Copyright © 2020 YamatoOtaka. All rights reserved.
+//
+
+import XCTest
+@testable import Sweetfish
+
+class SweetfishTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
