5b42042df3266420ca3a70a3733b89c94704c745
Yamato Otaka <yamato8010apple@gmail.com>
2020-04-16 22:05:06 +0900

    Refactor segmentation view

--- Sweetfish/Tool/SegmentationView.swift
+++ Sweetfish/Tool/SegmentationView.swift
@@ -9,15 +9,6 @@
 import UIKit
 
 final class SegmentationView: UIView {
-
-    func segmentationColor(with index: Int32) -> UIColor {
-        if index == 3 {
-            return UIColor.clear
-        } else {
-            return .white
-        }
-    }
-
     var segmentationmap: SegmentationResultMLMultiArray? = nil {
         didSet {
             self.setNeedsDisplay()
@@ -25,30 +16,36 @@ final class SegmentationView: UIView {
     }
 
     override func draw(_ rect: CGRect) {
-        
-        if let ctx = UIGraphicsGetCurrentContext() {
-            
-            ctx.clear(rect);
-            guard let segmentationmap = self.segmentationmap else { return }
-            
-            let size = self.bounds.size
-            let segmentationmapWidthSize = segmentationmap.segmentationmapWidthSize
-            let segmentationmapHeightSize = segmentationmap.segmentationmapHeightSize
-            let w = size.width / CGFloat(segmentationmapWidthSize)
-            let h = size.height / CGFloat(segmentationmapHeightSize)
-            
-            for j in 0..<segmentationmapHeightSize {
-                for i in 0..<segmentationmapWidthSize {
-                    let value = segmentationmap[j, i].int32Value
+        guard let context = UIGraphicsGetCurrentContext(), let segmentationmap = self.segmentationmap else { return }
+        context.clear(rect)
+        let size = bounds.size
+        let segmentationmapWidthSize = segmentationmap.segmentationmapWidthSize
+        let segmentationmapHeightSize = segmentationmap.segmentationmapHeightSize
+        let mappingWidth = size.width / CGFloat(segmentationmapWidthSize)
+        let mappingHeight = size.height / CGFloat(segmentationmapHeightSize)
 
-                    let rect: CGRect = CGRect(x: CGFloat(i) * w, y: CGFloat(j) * h, width: w*2, height: h*2)
+        for y in 0..<segmentationmapHeightSize {
+            for x in 0..<segmentationmapWidthSize {
+                let value = segmentationmap[y, x].int32Value
 
-                    let color: UIColor = segmentationColor(with: value)
+                let rect: CGRect = CGRect(x: CGFloat(x) * mappingWidth, y: CGFloat(y) * mappingHeight, width: mappingWidth*2, height: mappingHeight*2)
 
-                    color.setFill()
-                    UIRectFill(rect)
-                }
+                let color: UIColor = segmentationColor(with: value)
+
+                color.setFill()
+                UIRectFill(rect)
             }
+        }
+    }
+
+    private func segmentationColor(with index: Int32) -> UIColor {
+        if index == 3 {
+            return UIColor.clear
+        } else {
+            if let superviewBackgroundColor = superview?.backgroundColor, superviewBackgroundColor != .clear {
+                return superviewBackgroundColor
+            }
+            return .white
         }
     }
 }
