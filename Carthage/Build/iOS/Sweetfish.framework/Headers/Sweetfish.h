c02e8cdc70406927edd39a4b2b84d13824f0452c
Yamato Otaka <yamato8010apple@gmail.com>
2020-04-19 22:43:09 +0900

    Run carthage build --no-skip-current

---
+++ Carthage/Build/iOS/Sweetfish.framework/Headers/Sweetfish.h
@@ -0,0 +1,19 @@
+//
+//  Sweetfish.h
+//  Sweetfish
+//
+//  Created by 大高倭 on 2020/04/15.
+//  Copyright © 2020 YamatoOtaka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sweetfish.
+FOUNDATION_EXPORT double SweetfishVersionNumber;
+
+//! Project version string for Sweetfish.
+FOUNDATION_EXPORT const unsigned char SweetfishVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sweetfish/PublicHeader.h>
+
+
